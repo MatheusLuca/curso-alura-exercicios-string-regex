4d6198f2052fdd6f32146f7201f9ab11a4674985
Matheus Luca <luca_matheus@live.com>
2026-04-18 22:45:23 -0300

    Feat: Regex valor monetário e exiba o valor formatado com duas casas decimais.

--- src/StringExercicio6.java
+++ src/StringExercicio6.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class StringExercicio6 {
     static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Insira um valor monetário com bastante casas decimais: ");
+        String dinheiro = sc.nextLine();
+        System.out.println(dinheiro.replaceAll("^(\\d+,\\d{2})\\d+$", "$1"));
 
     }
 }
